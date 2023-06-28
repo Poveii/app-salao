25e8771142358e5915d35d2584276c45d276c27f
Poveii <yellowtutors@gmail.com>
2023-06-28 15:30:10 -0300

    feat: First Page, Welcome!

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-2 rounded-md bg-gray-200 p-8">
+        <h1 className="text-5xl">Olá Barbeiros!!</h1>
+        <p>Em breve novidades...</p>
+      </div>
+    </main>
+  )
+}
