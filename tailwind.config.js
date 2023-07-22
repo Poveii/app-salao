4175c75ee4d4a2ddfbd7041e1ae1dedf47a30b29
Poveii <yellowtutors@gmail.com>
2023-07-22 13:50:03 -0300

    feat: First color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          500: '#282828',
+          700: '#141414',
+          900: '#000000',
+        },
+        white: {
+          400: '#ffffff',
+          500: '#efefef',
+        },
+        gray: {
+          400: '#d9d9d9',
+          450: '#dbdbdb',
+          700: '#8f8f8f',
+        },
+        error: '#f23a5f',
+        success: '#3dd132',
+        ok: '#31e0e0',
+      },
+    },
   },
   plugins: [],
 }
